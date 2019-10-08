77a437612e3fb00b63dbb4a40dc2951b2877f8b2
miladrahimi <info@miladrahimi.com>
2019-10-08 13:13:52 +0330

    add some tests

--- env_test.go
+++ env_test.go
@@ -2,26 +2,31 @@ package env_test
 
 import (
 	"env"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
-func Test_Load_It_Should_Read_Empty_File(t *testing.T) {
-	vs, err := env.Load("test/.env1")
-	assert.NoErrorf(t, err, "Expected no error got %v", err)
-	assert.Empty(t, vs, "Expected empty env")
-	fmt.Println(vs)
+func Test_Load_It_Should_Return_Error_When_File_Not_Found(t *testing.T) {
+	vs, err := env.Load("test/.404")
+	assert.Nil(t, vs)
+	assert.Error(t, err)
 }
 
-func Test_Load_It_Should_Read_Env_File_And_Load_To_OS(t *testing.T) {
-	vs, err := env.Load("test/.env2")
+func Test_Load_It_Should_Read_Empty_File_And_Return_Empty_Map(t *testing.T) {
+	vs, err := env.Load("test/.empty.env")
+	assert.NoError(t, err)
+	assert.Empty(t, vs)
+}
+
+func Test_Load_It_Should_Read_And_Load_The_Sample_Env_File(t *testing.T) {
+	vs, err := env.Load("test/.env")
 	l := len(vs)
 
-	assert.NoErrorf(t, err, "Expected no error got %v", err)
+	assert.NoError(t, err)
 	assert.Equalf(t, 8, l, "Expected %v got %v", 8, l)
 
+	// Read
 	assert.Equal(t, "127.0.0.1", vs["DB_HOST"])
 	assert.Equal(t, "App", vs["DB_NAME"])
 	assert.Equal(t, "3306", vs["DB_PORT"])
@@ -31,6 +36,7 @@ func Test_Load_It_Should_Read_Env_File_And_Load_To_OS(t *testing.T) {
 	assert.Equal(t, "true", vs["DEBUG"])
 	assert.Equal(t, "#VALUE!", vs["NOT_COMMENT"])
 
+	// Load
 	assert.Equal(t, "127.0.0.1", os.Getenv("DB_HOST"))
 	assert.Equal(t, "App", os.Getenv("DB_NAME"))
 	assert.Equal(t, "3306", os.Getenv("DB_PORT"))
@@ -40,22 +46,28 @@ func Test_Load_It_Should_Read_Env_File_And_Load_To_OS(t *testing.T) {
 	assert.Equal(t, "#VALUE!", os.Getenv("NOT_COMMENT"))
 }
 
+func Test_Load_It_Should_Return_Error_When_File_Is_Invalid(t *testing.T) {
+	vs, err := env.Load("test/.buggy.env")
+	assert.Nil(t, vs)
+	assert.Error(t, err)
+}
+
 func Test_Load_It_Should_Not_Overwrite_OS_Variable(t *testing.T) {
 	err := os.Setenv("DB_NAME", "Not App!")
-	assert.NoErrorf(t, err, "Expected no error got %v", err)
+	assert.NoError(t, err)
 
-	_, err = env.Load("test/.env2")
-	assert.NoErrorf(t, err, "Expected no error got %v", err)
+	_, err = env.Load("test/.env")
+	assert.NoError(t, err)
 
 	assert.Equal(t, "Not App!", os.Getenv("DB_NAME"))
 }
 
 func Test_Overload_It_Should_Overwrite_OS_Variable(t *testing.T) {
 	err := os.Setenv("DB_NAME", "Not App!")
-	assert.NoErrorf(t, err, "Expected no error got %v", err)
+	assert.NoError(t, err)
 
-	_, err = env.Overload("test/.env2")
-	assert.NoErrorf(t, err, "Expected no error got %v", err)
+	_, err = env.Overload("test/.env")
+	assert.NoError(t, err)
 
 	assert.Equal(t, "App", os.Getenv("DB_NAME"))
 }
