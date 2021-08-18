c93170fd61834e069f02f377d5c6c597848b4b1c
Milad Rahimi <info@miladrahimi.com>
2021-08-18 23:30:20 +0430

    add doc block to methods

--- env.go
+++ env.go
@@ -1,3 +1,4 @@
+// Package env is a lightweight package for loading os environment variables into structs in Go projects.
 package env
 
 import (
@@ -8,6 +9,10 @@ import (
 	"unsafe"
 )
 
+// Load retrieves OS environment variables into the given struct.
+// It gets the pointer of a struct that is going to holds the variables.
+// The struct fields must have an `env` tag that determines the related OS environment variable.
+// Field example: IsAdmin bool `env:"IS_ADMIN"`
 func Load(structure interface{}) error {
 	receiverType := reflect.TypeOf(structure)
 
