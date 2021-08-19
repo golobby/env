082957865e2b2469240897af0a9be5d21a24c3d7
Milad Rahimi <info@miladrahimi.com>
2021-08-19 12:43:37 +0430

    support nested structs

--- env.go
+++ env.go
@@ -15,26 +15,39 @@ import (
 // Field example: IsAdmin bool `env:"IS_ADMIN"`
 func Load(structure interface{}) error {
 	receiverType := reflect.TypeOf(structure)
-
 	if receiverType != nil && receiverType.Kind() == reflect.Ptr {
 		elem := receiverType.Elem()
 		if elem.Kind() == reflect.Struct {
 			s := reflect.ValueOf(structure).Elem()
-			for i := 0; i < s.NumField(); i++ {
-				if t, exist := s.Type().Field(i).Tag.Lookup("env"); exist {
-					v, err := cast.FromString(os.Getenv(t), s.Type().Field(i).Type.Name())
-					if err != nil {
-						return err
-					}
-
-					ptr := reflect.NewAt(s.Field(i).Type(), unsafe.Pointer(s.Field(i).UnsafeAddr())).Elem()
-					ptr.Set(reflect.ValueOf(v))
-				}
-			}
-
-			return nil
+			return loadStruct(structure, s)
 		}
 	}
 
 	return errors.New("env: invalid structure")
+}
+
+func loadStruct(structure interface{}, s reflect.Value) error {
+	for i := 0; i < s.NumField(); i++ {
+		if t, exist := s.Type().Field(i).Tag.Lookup("env"); exist {
+			v, err := cast.FromString(os.Getenv(t), s.Type().Field(i).Type.Name())
+			if err != nil {
+				return err
+			}
+
+			ptr := reflect.NewAt(s.Field(i).Type(), unsafe.Pointer(s.Field(i).UnsafeAddr())).Elem()
+			ptr.Set(reflect.ValueOf(v))
+		} else if s.Type().Field(i).Type.Kind() == reflect.Struct {
+			if err := loadStruct(structure, s.Field(i)); err != nil {
+				return err
+			}
+		} else if s.Type().Field(i).Type.Kind() == reflect.Ptr {
+			if s.Field(i).IsZero() == false && s.Field(i).Elem().Type().Kind() == reflect.Struct {
+				if err := loadStruct(structure, s.Field(i).Elem()); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- env_test.go
+++ env_test.go
@@ -7,14 +7,23 @@ import (
 	"testing"
 )
 
+type Database struct {
+	Name string `env:"DB_NAME"`
+	Port int16  `env:"DB_PORT"`
+}
+
 type Sample struct {
 	NoEnv   float64 // A private field
-	private string  `env:"private"`  // A private field
-	Name    string  `env:"NAME"`     // A public field
+	private string  `env:"private"`  // A private string field
+	Name    string  `env:"NAME"`     // A string field
 	Number  int32   `env:"NUMBER"`   // A numeric field
 	Pi      float32 `env:"PI"`       // A float field
 	IsAdmin bool    `env:"IS_ADMIN"` // A boolean field
 	IsUser  bool    `env:"IS_USER"`  // A boolean field
+	MySQL   *Database
+	Nested  struct {
+		Number int32 `env:"NESTED_NUMBER"` // A numeric field
+	}
 }
 
 func TestLoad(t *testing.T) {
@@ -25,7 +34,13 @@ func TestLoad(t *testing.T) {
 	_ = os.Setenv("IS_ADMIN", "1")
 	_ = os.Setenv("IS_USER", "false")
 
+	_ = os.Setenv("NESTED_NUMBER", "33")
+
+	_ = os.Setenv("DB_NAME", "app")
+	_ = os.Setenv("DB_PORT", "666")
+
 	sample := Sample{}
+	sample.MySQL = &Database{}
 
 	err := env.Load(&sample)
 	assert.NoError(t, err)
@@ -36,6 +51,11 @@ func TestLoad(t *testing.T) {
 	assert.Equal(t, float32(3.14), sample.Pi)
 	assert.Equal(t, true, sample.IsAdmin)
 	assert.Equal(t, false, sample.IsUser)
+
+	assert.Equal(t, int32(33), sample.Nested.Number)
+
+	assert.Equal(t, "app", sample.MySQL.Name)
+	assert.Equal(t, int16(666), sample.MySQL.Port)
 }
 
 func TestLoad_With_Invalid_Structure_It_Should_Fail(t *testing.T) {
@@ -47,12 +67,44 @@ func TestLoad_With_Invalid_Structure_It_Should_Fail(t *testing.T) {
 
 func TestLoad_With_Invalid_Field_It_Should_Fail(t *testing.T) {
 	type structure struct {
-		Number bool `env:"NUMBER"`
+		Number int32 `env:"NUMBER"`
 	}
 
 	_ = os.Setenv("NUMBER", "invalid")
 
 	sample := structure{}
+	err := env.Load(&sample)
+	assert.Error(t, err)
+}
+
+func TestLoad_With_Invalid_Structure_Field_It_Should_Fail(t *testing.T) {
+	type structure struct {
+		Nested struct {
+			Number bool `env:"NUMBER"`
+		}
+	}
+
+	_ = os.Setenv("NUMBER", "invalid")
+
+	sample := structure{}
+	err := env.Load(&sample)
+	assert.Error(t, err)
+}
+
+func TestLoad_With_Invalid_Structure_Ptr_Field_It_Should_Fail(t *testing.T) {
+	type Nested struct {
+		Number bool `env:"NUMBER"`
+	}
+
+	type Structure struct {
+		Nested *Nested
+	}
+
+	_ = os.Setenv("NUMBER", "invalid")
+
+	sample := Structure{}
+	sample.Nested = &Nested{}
+
 	err := env.Load(&sample)
 	assert.Error(t, err)
 }
